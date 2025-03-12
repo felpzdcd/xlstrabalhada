be9166ad0dd07c4a5cb02d7a9dc3843d4b72f9e3
Felipe Correa de Sá <felipedesati@gmail.com>
2025-03-12 17:28:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,24 @@ import io
 
 def filtrar_pagamentos_numericos_e_termos(arquivo_excel, termos_excluir):
     try:
-        # Carregar o arquivo Excel
         df = pd.read_excel(arquivo_excel)
-
-        # Remover espaços extras no nome das colunas
         df.columns = df.columns.str.strip()
-
-        # Filtrar linhas onde a primeira célula contém os termos a excluir
         mascara_termos = ~df.iloc[:, 0].str.contains('|'.join(termos_excluir), case=False, na=False)
         df_sem_termos = df[mascara_termos]
-
-        # Filtrar onde a primeira célula é numérica
         df_filtrado = df_sem_termos[pd.to_numeric(df_sem_termos.iloc[:, 0], errors='coerce').notna()]
-
-        # Verificar se a coluna CPF/CNPJ existe
         possiveis_nomes = ["CPF/CNPJ", "CPF", "CNPJ", "cpf/cnpj", "Cpf/Cnpj"]
         nome_coluna = next((col for col in possiveis_nomes if col in df_filtrado.columns), None)
 
         if nome_coluna:
             df_filtrado[nome_coluna] = df_filtrado[nome_coluna].astype(str).str.strip()
-
-            # Adicionar zero à esquerda para CPF/CNPJ com 10 caracteres
             df_filtrado[nome_coluna] = df_filtrado[nome_coluna].apply(
                 lambda x: '0' + x if x.isdigit() and len(x) == 10 else x
             )
+            #Debug
+            for valor in df_filtrado[nome_coluna]:
+                if len(valor) == 11 and valor[0] == '0':
+                    print(f"valor com zero a esquerda:{valor}")
+
         else:
             st.error("Coluna 'CPF/CNPJ' não encontrada! Verifique se o nome está correto.")
 
@@ -39,13 +33,10 @@ def filtrar_pagamentos_numericos_e_termos(arquivo_excel, termos_excluir):
 
 def main():
     st.title("Filtragem de Pagamentos")
-
     usuario = st.text_input("Usuário")
     senha = st.text_input("Senha", type="password")
-
     if usuario == "tesouraria" and senha == "alcif0@":
         arquivo_excel = st.file_uploader("Carregue o arquivo Excel", type=["xls", "xlsx"])
-
         if arquivo_excel is not None:
             termos_excluir = [
                 'ITABUNA', 'Vencimento', 'Qtde de Reg.:', 'Total da Unidade', 'ALCIR - ITABUNA',
@@ -56,18 +47,14 @@ def main():
                 'FREI INOCÊNCIO', 'ALPERCATA', 'SUB - SPERANDIO SOLUCOES LTDA - C6',
                 'SUB - 3RN INTERME. DE NEGOCIOS LTDA - C6', 'SUB - SPERANDIO SOLUCOES LTDA - FACTA'
             ]
-
             df_filtrado = filtrar_pagamentos_numericos_e_termos(arquivo_excel, termos_excluir)
-
             if df_filtrado is not None:
                 st.write("DataFrame filtrado:")
                 st.dataframe(df_filtrado.astype(str))
-
                 output = io.BytesIO()
                 with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
                     df_filtrado.to_excel(writer, index=False)
                 output.seek(0)
-
                 st.download_button(
                     label="Baixar arquivo filtrado",
                     data=output,
